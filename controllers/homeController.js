dad4ed6863ed379c570aa9955c130dc8b006855e
Manohar <manohar20century@gmail.com>
2023-06-22 18:55:16 +0530

    rendering to login and register page from home page was success

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,18 @@
-module.exports.home = async (req,res)=>{
-    try{
-        return res.status(200).render('home',{
-            title : "Home"
+module.exports.home = async (req, res) => {
+    try {
+        return res.status(200).render('home', {
+            title: "Home"
         });
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return res.status(500).send(err);
     }
+}
+
+module.exports.signIn = (req, res) => {
+    return res.render('login', { title: 'Login' })
+}
+
+module.exports.signUp = (req, res) => {
+    return res.render('Register', { title: 'Register' })
 }
--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 
 router.get('/',homeController.home);
+router.get('/login',homeController.signIn);
+router.get('/register',homeController.signUp);
 
 module.exports = router;
