8b67e185a8b0b0f8af669962db3186666df916bd
Manohar <manohar20century@gmail.com>
2023-06-23 15:39:00 +0530

    made weekly view for all habits and send previous 6 days dates from server

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -12,12 +12,28 @@ module.exports.home = async (req, res) => {
             title: "Habit Tracker",
             user: user,
             habits: habits,
+            weeklyDates: getOneWeekDate()
         })
 
     } catch (err) {
         console.log(err);
         return res.status(500).send('Internal Server error');
     }
+}
+
+function getOneWeekDate(){
+    let months = ["", "Jan", "Feb", "March", "Apr", "May", "June", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    let dates = [];
+    for(let i=6;i>=0;i--){
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate()-i);
+        let mm = currentDate.getMonth() + 1;
+        mm = months[mm];
+        let dd =currentDate.getDate();
+        if(dd < 10) dd = '0' + dd;
+        dates.push(mm + ' ' + dd);
+    }
+    return dates;
 }
 
 module.exports.create = async (req, res) => {
