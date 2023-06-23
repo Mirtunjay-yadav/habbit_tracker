7ab78e3f9241fa5e694a91dad6867bef5a6182f1
Manohar <manohar20century@gmail.com>
2023-06-23 14:11:44 +0530

    openining and closing daily and weekly views done successfully

---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+function openForm(){
+    document.getElementById('habit-form').style.display = 'block';
+}
+
+function closeForm(){
+    document.getElementById('habit-form').style.display = "none";
+}
+
+function view(){
+    document.getElementById('view').style.display = "block";
+}
+
+function closeView(){
+    document.getElementById('view').style.display = "none";
+}
