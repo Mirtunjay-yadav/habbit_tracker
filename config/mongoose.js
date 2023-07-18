c8e99c9cc5f94990cb268c162c3532e64b2d3b15
Mirtunjay-yadav <129950944+Mirtunjay-yadav@users.noreply.github.com>
2023-07-18 18:41:53 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test1', {
+mongoose.connect('mongodb+srv://mirtunjayy435:Oz2jvHsSDE1fOOyw@cluster0.xni7vvx.mongodb.net/test1', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
